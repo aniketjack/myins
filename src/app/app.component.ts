71531d8329efcf29ead60ee8fb1c06d9cbeee4ca
Kamthe, Aniket <aniket.kamthe@accenture.com>
2020-04-21 18:27:24 +0530

    CSS look and feel update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { WaitingLoaderService } from './utilities/waiting-loader.service';
 import { Router } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+declare var Instamojo:any;
 
 @Component({
   selector: 'app-root',
@@ -14,7 +15,17 @@ export class AppComponent {
   showLoader: boolean;
   user:Object = {};
   isUserLoggedin: boolean = false;
+  // Payment status
+  _paymentFlgs = {
+    isPymentSuccessful : false,
+    isPymentFailed: false,
+    isPaymentCancelled: false,
+    paymentResponse:{}
+  };
   
+  //Instamojo = (<any>window['Instamojo']);
+  //Instamojo = (<any>window['PaymentGateway']);
+
   constructor(
     private waitingLoaderService: WaitingLoaderService,
     private router: Router,
@@ -27,29 +38,78 @@ export class AppComponent {
     this.waitingLoaderService.status.subscribe((val: boolean) => {
       this.showLoader = val;
     });
-    this.router.events.subscribe((res) => {
-      if (this.router.url === '/login' || this.router.url === '/signup') {
-        this.isUserLoggedin = false
-      } else {
-        this.isUserLoggedin = true
-      }
-    })
+      this.router.events.subscribe((res) => {
+        if (this.router.url === '/login' || this.router.url === '/signup') {
+          this.isUserLoggedin = false
+        } else {
+          this.isUserLoggedin = true
+        }
+      })
+     // console.log("Window Variables >>>>> ", (<any>window)._paymentFlgs);
+    
+  }
 
-    // http post request
-    //https://slixueelx1.execute-api.us-east-1.amazonaws.com/Beta
-    let content = JSON.stringify({isPaymentGateway: true});
-    let headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Token': 'sarthi1' });
-  let options = { headers: headers };
-    //headers.append('', 'sarthi1');
-   // headers = headers.append('content')
-    this.http.post("https://slixueelx1.execute-api.us-east-1.amazonaws.com/Beta", content,options).subscribe(res=>{
-      console.log("Lex response >>> ", res);
-    },err=>{
-      console.log("Error in API")
-    })
+  /********************************************************************************************************
+   * doPayment()
+   *********************************************************************************************************/
+  doPayment(){
+
+    let thisRef = this;
+    let status = false, paymentResponse =  {};
+          function onOpenHandler () {
+            //alert('Payments Modal is Opened');
+            console.log('Payment modal is getting open');
+          } 
+
+          function onCloseHandler () {
+            if(status){
+              this.sendFlagToBot({isPaymentSuccess: true, paymentDetails:  paymentResponse});
+            }
+            
+          }
+
+          function onPaymentSuccessHandler (response) {
+             status = true;
+             paymentResponse = response;
+          }
+
+          function onPaymentFailureHandler (response) {
+            status = true;
+            paymentResponse = response;
+          }
+          /* End client-defined Callback Handler Functions */
+
+          /* Configuring Handlers */
+        Instamojo.configure({
+            handlers: {
+              onOpen: onOpenHandler,
+              onClose: onCloseHandler,
+              onSuccess: onPaymentSuccessHandler,
+              onFailure: onPaymentFailureHandler
+            },
+            directPaymentMode: 'credit card'
+          });
+
+          Instamojo.open('https://test.instamojo.com/@ackamthe');
+          
 
   }
 
+
+  /*********************************************************************************************************
+   * Post Payment bot communication activity. To send a pyment gteway flag to bot
+   * 
+   *********************************************************************************************************/
+  sendFlagToBot(obj){
+    let content = JSON.stringify(obj);
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Token': 'sarthi1' });
+    let options = { headers: headers };
+    this.http.post("https://slixueelx1.execute-api.us-east-1.amazonaws.com/Beta", content,options).subscribe(res=>{
+        console.log("Beta API Response >>>> ", res);
+      },err=>{
+        console.log("Error in Beta API >>> ", JSON.stringify(err));
+      })
+  }
 }
